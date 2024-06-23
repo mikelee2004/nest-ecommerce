18658dd98292b2583fcc275c81e7ae8d2aefb409
mikelee2004 <strober2004@gmail.com>
2024-06-23 19:38:50 +0300

    config module setup + env file with port and server

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  const PORT = parseInt(process.env.PORT);
+  const SERVER = process.env.SERVER;
+  await app.listen(PORT, SERVER);
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap();
