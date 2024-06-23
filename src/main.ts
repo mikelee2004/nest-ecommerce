83accb02f42229b390e9ce6a9ecf742b7a2ce84d
mikelee2004 <strober2004@gmail.com>
2024-06-23 19:47:44 +0300

    swagger.ui setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  // swagger ui
+  const config = new DocumentBuilder()
+  .setTitle('E-commerce Application')
+  .setDescription(
+    '[The source API definition (json)](http://${process.env.SERVER}:${process.env.PORT}/api-json)'
+  )
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const PORT = parseInt(process.env.PORT);
   const SERVER = process.env.SERVER;
   await app.listen(PORT, SERVER);
